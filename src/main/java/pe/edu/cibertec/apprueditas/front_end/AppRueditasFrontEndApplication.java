84a33cd573c169b83ecbb59fe89360a06ed5e29a
Gad Josue Alva Castromonte <josuealva920@gmail.com>
2024-09-24 22:50:13 -0500

    creacion del proyecto con spring initializr

---
+++ src/main/java/pe/edu/cibertec/apprueditas/front_end/AppRueditasFrontEndApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.apprueditas.front_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRueditasFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRueditasFrontEndApplication.class, args);
+	}
+
+}
